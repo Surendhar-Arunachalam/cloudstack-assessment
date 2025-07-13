34673235ee28aa77683a26ac2219976962c01e63
Surendhar-Arunachalam <159416822+Surendhar-Arunachalam@users.noreply.github.com>
2025-07-13 15:38:27 +0530

    Update api_test.py
    updated

--- api_test.py
+++ api_test.py
@@ -5,7 +5,7 @@ url = "http://127.0.0.1:5000/linkedin/connections"
 payload = {
     'username': 'test@gmail.com',
     'password': 'test1234',
-    'session_persistence': 'True'  # True|False
+    'mfa_key': 'ZZC6C6W563QK2FSH64FAHBURHTKX27CD'
 }
 
 response = requests.request("POST", url, data=payload)
